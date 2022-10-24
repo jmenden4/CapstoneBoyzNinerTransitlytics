80478510e7e666010962c8ecdfc43ee1466895f6
jmenden4 <jmenden4@uncc.edu>
2022-10-24 18:01:52 -0400

    update deploy scripts

--- scripts/deploy-server.sh
+++ scripts/deploy-server.sh
@@ -1,6 +1,6 @@
 pyclean --verbose server/src
-ssh -i ../admin.pem ec2-user@ec2-3-144-148-29.us-east-2.compute.amazonaws.com 'rm -rf /home/ec2-user/app/server/src'
-scp -i ../admin.pem -r server/src ec2-user@ec2-3-144-148-29.us-east-2.compute.amazonaws.com:/home/ec2-user/app/server/src
-scp -i ../admin.pem -r server/requirements.txt ec2-user@ec2-3-144-148-29.us-east-2.compute.amazonaws.com:/home/ec2-user/app/server
-scp -i ../admin.pem -r server/gunicorn_config.py ec2-user@ec2-3-144-148-29.us-east-2.compute.amazonaws.com:/home/ec2-user/app/server
-ssh -i ../admin.pem ec2-user@ec2-3-144-148-29.us-east-2.compute.amazonaws.com 'sudo systemctl restart ninerlytics-api.service'
+ssh -i ../admin.pem ec2-user@transit-ninerlytics.com 'rm -rf /home/ec2-user/app/server/src'
+scp -i ../admin.pem -r server/src ec2-user@transit-ninerlytics.com:/home/ec2-user/app/server/src
+scp -i ../admin.pem -r server/requirements.txt ec2-user@transit-ninerlytics.com:/home/ec2-user/app/server
+scp -i ../admin.pem -r server/gunicorn_config.py ec2-user@transit-ninerlytics.com:/home/ec2-user/app/server
+ssh -i ../admin.pem ec2-user@transit-ninerlytics.com 'sudo systemctl restart ninerlytics-api.service'
