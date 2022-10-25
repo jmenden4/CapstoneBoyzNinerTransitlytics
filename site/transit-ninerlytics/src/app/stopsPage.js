f18b4740fc71b6ad5dde7856d97d104aa1bb93b5
jmenden4 <jmenden4@uncc.edu>
2022-10-25 12:16:03 -0400

    formatting

--- site/transit-ninerlytics/src/app/stopsPage.js
+++ site/transit-ninerlytics/src/app/stopsPage.js
@@ -67,9 +67,7 @@ const StopsPage = () => {
                     <Dropdown.Menu className="w-100">
                         <Dropdown.Item eventKey="1">Action</Dropdown.Item>
                         <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
-                        <Dropdown.Item eventKey="3" active>
-                        Active Item
-                        </Dropdown.Item>
+                        <Dropdown.Item eventKey="3" active>Active Item</Dropdown.Item>
                         <Dropdown.Divider />
                         <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
                     </Dropdown.Menu>
