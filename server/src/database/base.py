d92f966df9792460670b083789c78ba91fe35897
jmenden4 <jmenden4@uncc.edu>
2022-11-13 12:50:44 -0500

    use .env file

--- server/src/database/base.py
+++ server/src/database/base.py
@@ -1,19 +1,35 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import sshtunnel
+import os
+from pydantic import BaseSettings
 
-server = sshtunnel.SSHTunnelForwarder(
-    "transit-ninerlytics.com",
-    ssh_username="ec2-user",
-    ssh_pkey=r"C:\Users\Jon\Documents\School\Fall 2022\ITCS-4155\admin.pem",
-    remote_bind_address=("database-2.ccxeki6fimht.us-east-2.rds.amazonaws.com", 3306),
-    local_bind_address=("127.0.0.1", 5445),
-)
-server.start()
 
-SQLALCHEMY_DB_URL = "mysql://admin:CapstoneBoyz@localhost:5445/transitninerlytics"
-# SQLALCHEMY_DB_URL = "mysql://datauser:datauser@localhost/transitninerlytics"
-engine = create_engine(SQLALCHEMY_DB_URL)
+class Settings(BaseSettings):
+    ENVIRONMENT: str
+    DB_SSH_KEY_PATH: str = None
+    DB_CONNECTION_URL: str
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
+
+
+
+if settings.ENVIRONMENT == "local":
+    server = sshtunnel.SSHTunnelForwarder(
+        "transit-ninerlytics.com",
+        ssh_username="ec2-user",
+        ssh_pkey=settings.DB_SSH_KEY_PATH,
+        remote_bind_address=("database-2.ccxeki6fimht.us-east-2.rds.amazonaws.com", 3306),
+        local_bind_address=("127.0.0.1", 5445),
+    )
+    server.start()
+
+
+engine = create_engine(settings.DB_CONNECTION_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
