ba85ab619444e851f688d03d3f99e341971ea276
dbarne42 <89850056+dbarne42@users.noreply.github.com>
2022-11-14 14:07:26 -0500

    Update main.py
    Added get methods

--- server/src/main.py
+++ server/src/main.py
@@ -16,9 +16,52 @@ root_router = APIRouter(prefix="/api")
 
 # GET /api/buses - get list of buses (all)
 
+class BusSchema(BaseModel):
+    id: int
+    code: int
+
+@root_router.get("/data/bus", response_model=List[BusSchema])
+def fetch_routes(
+    db: Session = Depends(get_db_session),
+):
+    results = db.query(
+        Bus.id.label("id"),
+        Route.code.label("Route")).all()
+
+    return results
+
 # GET /api/routes - get list of routes (all)
 
+class RouteSchema(BaseModel):
+    id: int
+    name: str
+
+@root_router.get("/data/routes", response_model=List[RouteSchema])
+def fetch_routes(
+    db: Session = Depends(get_db_session),
+):
+    results = db.query(
+        Route.id.label("id"),
+        Route.name.label("route")).all()
+
+    return results
+
 # GET /api/stops - get list of stops (all)
+
+class StopSchema(BaseModel):
+    id: int
+    stop: str
+    
+@root_router.get("/data/stops", response_model=List[StopSchema])
+def fetch_routes(
+    db: Session = Depends(get_db_session),
+):
+    results = db.query(
+        Stop.id.label("id"),
+        Stop.stop.label("bus_id")).all()
+
+    return results
+
 
 
 
@@ -78,9 +121,38 @@ def fetch_stop_info(
     return results
 
 
-
-
 # GET /api/data/businfo - get travel distance for buses (with filters in JSON body)
+class BusInfoSchema(BaseModel):
+    id: int
+    num_times_stopped: int
+    total_people_on: int
+    total_people_off: int
+    avg_wait_time: float
+    min_wait_time: int
+    max_wait_time: int
+
+
+@root_router.post("/data/businfo", response_model=List[BusInfoSchema])
+def fetch_stop_info(
+    filters: DataFilterSchema,
+    db: Session = Depends(get_db_session),
+):
+    results = db.query(
+        StopData.stop.label("id"),
+        func.count(StopData.id).label("num_times_stopped"),
+        func.sum(StopData.num_people_on).label("total_people_on"),
+        func.sum(StopData.num_people_off).label("total_people_off"),
+        func.avg(StopData.wait_time).label("avg_wait_time"),
+        func.min(StopData.wait_time).label("min_wait_time"),
+        func.max(StopData.wait_time).label("max_wait_time"),
+    ).select_from(StopData).group_by(StopData.route).where(
+        StopData.date.between(filters.min_date, filters.max_date),
+        StopData.time.between(filters.min_time, filters.max_time),
+        StopData.bus.in_(filters.bus_ids),
+        StopData.route.in_(filters.route_ids),
+    ).all()
+
+    return results
 
 
 @root_router.get("/items/{item_id}")
